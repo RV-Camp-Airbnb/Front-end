77728494593081c39d12e56a5e02f4d8e4536072
ignaciosm <ignaciosm@gmail.com>
2019-11-19 10:16:59 -0500

    create dummy signup form

--- rv-front-end/src/components/Navbar.js
+++ rv-front-end/src/components/Navbar.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import {Route, Link} from "react-router-dom";
+import UserRegistration from './UserRegistration';
+import styled from 'styled-components'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <NavContainer>
+      <Logo>RV Camp</Logo>
+      <Wrapper>
+        <StyledLink>
+          <Link to="/host">Host</Link>
+        </StyledLink>
+        <StyledLink>
+          <Link to="/signup">Sign up</Link>
+        </StyledLink>
+        <StyledLink>
+          <Link to="/login">Log in</Link>
+        </StyledLink>
+      </Wrapper>
+    </NavContainer>
+
+    <Route path='/signup' component={UserRegistration} />
+    </>
   )
 }
 
 export default Navbar
+
+
+// STYLED COMPONENTS
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-decoration: none;
+  width: 80%;
+  margin: 0 auto;
+`
+const Logo = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  font-size: 2rem;
+`
+const Wrapper = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  text-decoration: none;
+  width: 60%;
+`
+const StyledLink = styled(Link)`
+  display:flex;
+  flex-direction: column;
+  align-items: space-between;
+`;
--- rv-front-end/src/components/UserRegistration.js
+++ rv-front-end/src/components/UserRegistration.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styled from 'styled-components'
 
 const UserRegistration = () => {
+  const [user, setUser] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    zipcode: ''
+  })
+
+  const handleChanges = e => {
+    console.log('changing', e.target.value)
+    setUser({
+      ...user,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('submitted!')
+
+  }
+
+  console.log('user', user)
+
   return (
-    <div>
+    <Wrapper>
+      <Form onSubmit={handleSubmit} >
+        <Input type="text" name='firstname' placeholder='First name' value={user.firstname} onChange={handleChanges} />
+        <Input type="text" name='lastname' placeholder='Last name' value={user.lastname} onChange={handleChanges} />
+        <Input type="email" name='email' placeholder='Email' value={user.email} onChange={handleChanges} />
+        <Input type="password" name='password' placeholder='Password' value={user.password} onChange={handleChanges} />
+        <Input type="text" name='zipcode' placeholder='Zip code' value={user.zipcode} onChange={handleChanges} />
+        <Button>Join RV Camp</Button>
+      </Form>
       
-    </div>
+    </Wrapper>
   )
 }
 
 export default UserRegistration
+
+// STYLED COMPONENTS
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+  width: 40%;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-decoration: none;
+  width: 100%;
+  padding: 2%;
+`
+
+const Input = styled.input`
+  padding: 2%;
+  margin: 2%;
+  font-size: 1.5rem;
+  border-radius: 5px;
+`
+
+const Button = styled.button`
+  padding: 2%;
+  margin: 2%;
+  background: tomato;
+  font-size: 1.5rem;
+  border-radius: 5px;
+`
